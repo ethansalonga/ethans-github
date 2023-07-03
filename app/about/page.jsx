d86515c0064950dcaceca33a72047a9ce578bbd3
ethansalonga <ethan@ethansalonga.dev>
2023-07-03 19:13:59 -0400

    first commit

---
+++ app/about/page.jsx
@@ -0,0 +1,17 @@
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Traversy Media</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci
+        voluptatibus dolorem molestias deleniti ipsa nobis repudiandae harum a
+        quas maxime consectetur accusamus consequatur iure et, possimus facere
+        omnis placeat culpa aliquid iusto modi ab! Soluta, accusamus commodi.
+        Tempora facere debitis, labore nisi molestias minima animi corporis id
+        deleniti fugit nemo.
+      </p>
+    </div>
+  )
+}
+
+export default AboutPage

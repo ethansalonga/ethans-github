8a6b86a976939b32bf0148b5aede2482aee8be7d
Ethan Salonga <ethansalonga28@gmail.com>
2023-07-04 16:52:33 -0400

    add team nested route

---
+++ app/about/team/page.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+const TeamPage = () => {
+  return (
+    <div>
+      <h1>Our Team</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto vitae
+        repellendus aut obcaecati! Laboriosam dicta, adipisci at aperiam
+        possimus, cumque corrupti culpa quia quidem corporis mollitia numquam!
+        Laborum, cupiditate corporis.
+      </p>
+    </div>
+  )
+}
+
+export default TeamPage
